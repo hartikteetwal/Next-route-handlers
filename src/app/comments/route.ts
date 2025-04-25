51af5e7819e69ce03a5726892a6eaafd69bc08fd
hartik teetwal <hartik.teetwal@ezbizsoft.in>
2025-04-25 18:34:39 +0530

    changes

--- src/app/comments/route.ts
+++ src/app/comments/route.ts
@@ -1,7 +1,14 @@
 import { comments } from "./data";
+import { type NextRequest } from "next/server";
 
-export async function GET() {
-    return Response.json(comments)
+export async function GET(req:NextRequest) {
+
+    const searchParams = req.nextUrl.searchParams;
+    const query = searchParams.get("query")
+
+    const filteredComments = query?comments.filter((comment)=>comment.text.includes(query)):comments
+
+    return Response.json(filteredComments)
 }
 
 export async function POST(req:Request) {
--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,19 @@
-export async function GET() {
-    return new Response("Fetch profile api data")
+import { type NextRequest } from "next/server"
+import { headers } from "next/headers"
+
+
+
+// ----> type 1
+// export async function GET(req: NextRequest) {
+//     const requestHeaders = new Headers(req.headers)
+//     console.log(requestHeaders.get("Authorization"))
+//     return new Response("Fetch profile api data")
+// }
+
+
+// ----> type 2
+export async function GET(req: NextRequest) {
+    const headerList = await headers();
+    console.log(headerList.get("Authorization"))
+    return new Response("<h1>fetch profile api data</h1>")
 }
